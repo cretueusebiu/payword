497860eac2073fe585b7ab7c45e0421d94792502
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-01-07 23:54:46 +0200

    Add method to get commits by user identity.

--- app/Models/Commit.php
+++ app/Models/Commit.php
@@ -9,10 +9,20 @@ class Commit extends Model
     /**
      * @var array
      */
-    protected $fillable = ['user_identity', 'commit', 'last_payword', 'page_number'];
+    protected $fillable = ['user_identity', 'commit', 'last_payword', 'page_id'];
 
     /**
      * @var array
      */
-    protected $casts = ['id' => 'integer', 'page_number' => 'integer'];
+    protected $casts = ['id' => 'integer', 'page_id' => 'integer'];
+
+    /**
+     * Find commits by user identity.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public static function findByUserIdentity($identity)
+    {
+        return static::where('user_identity', $identity)->get();
+    }
 }
